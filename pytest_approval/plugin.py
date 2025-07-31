1583b8d3bfe7598b2ad0e9e6b7da1ea020d0ec27
matthias_schaub <matthias.schaub@posteo.de>
2025-07-31 08:56:06 +0200

    add dummy pytest plugin entrypoint (wip)

---
+++ pytest_approval/plugin.py
@@ -0,0 +1,30 @@
+import logging
+
+import pytest
+
+node_ids = []
+
+
+@pytest.hookimpl
+def pytest_collection_modifyitems(items):
+    global node_ids
+    for i in items:
+        node_ids.append(i.nodeid)
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """Cleanup unused approval files."""
+    if exitstatus != 0:
+        return
+    config = session.config
+    args = config.invocation_params.args
+    if args:
+        logging.debug("Specific tests/files were targeted. Abort cleanup.")
+        return
+    else:
+        logging.debug("Whole test suite has been targeted. Start cleanup.")
+    # TODO:
+        # 1. Search for all approve/received files
+        # 2. Generate file names for each node_id
+        # 3. Delete all approved/received files (1)
+            # which are not part of generated files names (2)
