3ee8d8f12df3e32d0debfd72091eedaba8f7a3dd
matthias_schaub <matthias.schaub@posteo.de>
2025-08-28 07:57:44 +0200

    tests: add scripts dir

---
+++ scripts/generate.py
@@ -0,0 +1,26 @@
+from PIL import Image, PngImagePlugin
+import numpy as np
+
+
+# Create two simple black and white images with different sizes
+def create_test_images():
+    # Image 1: 100x100 pixels
+    img1 = Image.new("L", (100, 100), color=0)  # Black image
+
+    # Image 2: 200x200 pixels with same visual content (scaled)
+    img2 = Image.new("L", (100, 100), color=0)  # Black image
+
+    # Add different metadata
+    meta1 = PngImagePlugin.PngInfo()
+    meta1.add_text("Title", "Test Image 1")
+    meta1.add_text("Author", "Generator")
+
+    # Save with different metadata
+    img1.save("image1.png", pnginfo=meta1)
+    img2.save("image2.png")
+
+
+create_test_images()
+import filecmp
+
+print(filecmp.cmp("image1.png", "image2.png", shallow=False))
