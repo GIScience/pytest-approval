115134df15c7ae80ac30a21d63eda43c737cda52
matthias_schaub <matthias.schaub@posteo.de>
2025-09-04 08:10:05 +0200

    style: run ruff

--- pytest_approval/plugin.py
+++ pytest_approval/plugin.py
@@ -8,14 +8,20 @@ from pytest_approval import main
 node_ids = []
 auto_approve: bool = False
 
+
 def pytest_addoption(parser):
     parser.addoption(
         "--auto-approve",
         action="store_true",
         help="Automatically approve every approval test",
     )
-    parser.addoption("--approved-dir", action="store", default="", help="Directory for approved files "
-                                                                                      "(relative to pytest root.")
+    parser.addoption(
+        "--approved-dir",
+        action="store",
+        default="",
+        help="Directory for approved files (relative to pytest root.",
+    )
+
 
 def pytest_configure(config):
     main.ROOT_DIR = config.rootpath
@@ -24,6 +30,7 @@ def pytest_configure(config):
         main.APPROVED_DIR = approved_dir
         approved_dir_path = Path(main.ROOT_DIR) / Path(main.APPROVED_DIR)
         approved_dir_path.mkdir(parents=True, exist_ok=True)
+
 
 @pytest.hookimpl
 def pytest_collection_modifyitems(items):
