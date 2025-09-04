f17d51b112722ce48f30bb92d1cb133dd443cb87
Levi Szamek <levi.szamek@heigit.org>
2025-09-04 09:26:35 +0200

    fixup

--- pytest_approval/plugin.py
+++ pytest_approval/plugin.py
@@ -16,8 +16,8 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     main.ROOT_DIR = config.rootpath
-
-    if approved_dir := CONFIG.get("approved-dir", None):
+    approved_dir = CONFIG.get("approved-dir", None)
+    if approved_dir is not None:
         main.APPROVED_DIR = approved_dir
         approved_dir_path = Path(main.ROOT_DIR) / Path(main.APPROVED_DIR)
         approved_dir_path.mkdir(parents=True, exist_ok=True)
