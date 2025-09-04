2557f9a936fa722dd1e2790b89d22a87519c5540
Levi Szamek <levi.szamek@heigit.org>
2025-09-04 09:08:29 +0200

    feat: use config field for approved-dir

--- pytest_approval/plugin.py
+++ pytest_approval/plugin.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pytest_approval import main
+from pytest_approval.definitions import CONFIG
 
 auto_approve: bool = False
 
@@ -11,18 +12,12 @@ def pytest_addoption(parser):
         action="store_true",
         help="Automatically approve every approval test",
     )
-    parser.addoption(
-        "--approved-dir",
-        action="store",
-        default="",
-        help="Directory for approved files (relative to pytest root.",
-    )
 
 
 def pytest_configure(config):
     main.ROOT_DIR = config.rootpath
-    main.AUTO_APPROVE = config.getoption("--auto-approve")
-    if approved_dir := config.getoption("--approved-dir", None):
+
+    if approved_dir := CONFIG.get("approved-dir", None):
         main.APPROVED_DIR = approved_dir
         approved_dir_path = Path(main.ROOT_DIR) / Path(main.APPROVED_DIR)
         approved_dir_path.mkdir(parents=True, exist_ok=True)
