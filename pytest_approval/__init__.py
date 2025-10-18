009d21ebefda3675cc0de4a329ca7c854a4f7b66
Matthias Schaub <matthias.schaub@heigit.org>
2025-10-18 09:10:08 +0200

    fix: import test_verify_image_pillow only if pillow is installed

--- pytest_approval/__init__.py
+++ pytest_approval/__init__.py
@@ -1,15 +1,30 @@
-from pytest_approval.main import (
-    verify,
-    verify_binary,
-    verify_image,
-    verify_image_pillow,
-    verify_json,
-)
+try:
+    from pytest_approval.main import (
+        verify,
+        verify_binary,
+        verify_image,
+        verify_image_pillow,
+        verify_json,
+    )
 
-__all__ = (
-    "verify",
-    "verify_binary",
-    "verify_image",
-    "verify_image_pillow",
-    "verify_json",
-)
+    __all__ = (
+        "verify",
+        "verify_binary",
+        "verify_image",
+        "verify_image_pillow",
+        "verify_json",
+    )
+except ImportError:
+    from pytest_approval.main import (
+        verify,
+        verify_binary,
+        verify_image,
+        verify_json,
+    )
+
+    __all__ = (
+        "verify",
+        "verify_binary",
+        "verify_image",
+        "verify_json",
+    )
