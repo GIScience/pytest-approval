6bca2f96c0af3f6caf133a7305cce7d532f6ff5d
Matthias Schaub <matthias.schaub@heigit.org>
2025-10-22 10:51:01 +0200

    fix(compare): do not through exception if shapes are different
    instead return False

--- pytest_approval/compare.py
+++ pytest_approval/compare.py
@@ -17,6 +17,16 @@ def compare_files(received: Path, approved: Path) -> bool:
         return False
 
 
+def compare_files_shallow(received: Path, approved: Path) -> bool:
+    logger.debug(f"Compare {received} with {approved}.")
+    if filecmp.cmp(received, approved, shallow=True):
+        return True
+    elif received.suffix not in BINARY_EXTENSIONS:
+        return approved.read_text() == received.read_text()
+    else:
+        return False
+
+
 def compare_image_contents_only(received: Path, approved: Path) -> bool:
     """Compare image contents without metadata."""
     try:
@@ -29,4 +39,6 @@ def compare_image_contents_only(received: Path, approved: Path) -> bool:
         ) from error
     received_image = Image.open(received)
     approved_image = Image.open(approved)
-    return bool((numpy.array(received_image) == numpy.array(approved_image)).all())
+    received_array = numpy.array(received_image)
+    approved_array = numpy.array(approved_image)
+    return numpy.array_equiv(received_array, approved_array)
--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,7 +1,10 @@
 import pytest
 from PIL import Image, PngImagePlugin
 
-from pytest_approval.compare import compare_files, compare_image_contents_only
+from pytest_approval.compare import (
+    compare_files,
+    compare_image_contents_only,
+)
 
 
 @pytest.fixture
@@ -34,6 +37,17 @@ def images_almost_equal(tmp_path):
     return (tmp_path / "image1.png", tmp_path / "image2.png")
 
 
+@pytest.fixture
+def images_unequal_dimensions(tmp_path):
+    """Two equal images with the different metadata."""
+    img1 = Image.new("L", (100, 101), color=0)  # Black image
+    img2 = Image.new("L", (101, 100), color=0)
+
+    img1.save(tmp_path / "image1.png")
+    img2.save(tmp_path / "image2.png")
+    return (tmp_path / "image1.png", tmp_path / "image2.png")
+
+
 def test_image_compare(images_equal):
     received, approved = images_equal
     assert compare_files(received, approved) is True
@@ -50,3 +64,9 @@ def test_image_compare_almost_equal(images_almost_equal):
     received, approved = images_almost_equal
     assert compare_files(received, approved) is False
     assert compare_image_contents_only(received, approved) is True
+
+
+def test_image_compare_unequal_dimensions(images_unequal_dimensions):
+    received, approved = images_unequal_dimensions
+    assert compare_files(received, approved) is False
+    assert compare_image_contents_only(received, approved) is False
