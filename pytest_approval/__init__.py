57858a1388bddcb1daf7724027cf994dfa286b40
Matthias Schaub <matthias.schaub@heigit.org>
2026-01-26 14:48:47 +0100

    fix: separately try to import verify_plotly and verify_image_pillow

--- pytest_approval/__init__.py
+++ pytest_approval/__init__.py
@@ -1,38 +1,30 @@
+from contextlib import suppress
+
+from pytest_approval.main import (
+    verify,
+    verify_binary,
+    verify_image,
+    verify_json,
+)
 from pytest_approval.scrub import get_datetime_scrubber, get_uuid_scrubber
 
-try:
-    from pytest_approval.main import (
-        verify,
-        verify_binary,
-        verify_image,
-        verify_image_pillow,
-        verify_json,
-        verify_plotly,
-    )
+__all__ = (
+    "get_datetime_scrubber",
+    "get_uuid_scrubber",
+    "verify",
+    "verify_binary",
+    "verify_image",
+    "verify_json",
+)
 
-    __all__ = (
-        "get_datetime_scrubber",
-        "get_uuid_scrubber",
-        "verify",
-        "verify_binary",
-        "verify_image",
-        "verify_image_pillow",
-        "verify_json",
-        "verify_plotly",
-    )
-except ImportError:
-    from pytest_approval.main import (
-        verify,
-        verify_binary,
-        verify_image,
-        verify_json,
-    )
 
-    __all__ = (
-        "get_datetime_scrubber",
-        "get_uuid_scrubber",
-        "verify",
-        "verify_binary",
-        "verify_image",
-        "verify_json",
-    )
+with suppress(ImportError):
+    from pytest_approval.main import verify_image_pillow
+
+    __all__ = (*__all__, "verify_image_pillow")
+
+
+with suppress(ImportError):
+    from pytest_approval.main import verify_plotly
+
+    __all__ = (*__all__, "verify_plotly")
