6e0ff0a142ae7eff9cfcd78c1497da4ba567c604
Matthias Schaub <matthias.schaub@heigit.org>
2026-04-21 08:56:07 +0200

    fix: call function

--- pytest_approval/main.py
+++ pytest_approval/main.py
@@ -156,7 +156,7 @@ if PLOTLY_AVAILABLE:
 
         # First verify JSON without reporting (Compare JSON)
         success = _verify(data_json, extension=".json", report_suppress=True)
-        if (success and not report_always) or is_continuous_environment:
+        if (success and not report_always) or is_continuous_environment():
             return success
 
         # Second verify Image with reporting (Report image) if JSON is different
